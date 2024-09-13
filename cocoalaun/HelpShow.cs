047cd26d47145e2f66dbcac58ac3c39dd8a791f7
Kabancik166 <kabancik166@gmail.com>
2024-09-13 17:50:11 +0300

    Basic

---
+++ cocoalaun/HelpShow.cs
@@ -0,0 +1,22 @@
+namespace CocoaLaun;
+
+public class HelpShow
+{
+    public static void ShowHelp()
+    {
+        Console.WriteLine("Welcome to Help Cocoa Laun!");
+        Console.WriteLine("\n Global Commands: ");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine(" -$  cocoalaun help   (Help to cocoalaun) \n -$  cocoalaun -settings   (Global Settings :/) \n -$  cocoalaun -version   (View version) \n -$  cocoalaun -stopall   (Save and stop all servers)");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine(" Server Global Commands: ");
+        Console.WriteLine(" -$  cocoalaun servers -list   (list servers) \n -$  cocoalaun servers --{id server} -open   (Open directory server) \n -$  cocoalaun servers -new   (Create new Server) \n -$  cocoalaun servers -import   (Importing Server) \n -$  cocoalaun servers --{server-id} -export   (Export {server-id}.zip to /home)  \n -$  cocoalaun servers --{id server} -delete   (Delete server) \n -$  cocoalaun servers -deleteall   (Delete all servers) \n cocoalaun servers -settings   (Global settings servers)");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine(" Server Editor Commands:  ");
+        Console.WriteLine(" -$  cocoalaun servers editor --{id server}   (Menu Editor Server) \n -$  cocoalaun servers editor -leave   (Leave is Editor Server) \n -$  cocoalaun edit --{num}   (Editor num)");
+        Console.WriteLine("--------------------------------------------------");
+        
+        Console.WriteLine("\n If you have any bugs/questions/ideas, feel free to comment on this project on Github ");
+        Console.WriteLine("\n By Kabancik166.");
+    }
+}
---
+++ cocoalaun/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CocoaLaun;
+
+class Program
+{
+    public static string versi = " beta 0.0.1";
+    
+    static void Main(string[] args)
+    {
+        // Проверка наличия аргументов
+        if (args.Length == 0)
+        {
+            ShowWelcomeMessage();
+        }
+        else
+        {
+            // Обработка переданных аргументов
+            switch (args[0].ToLower())
+            {
+                case "help":
+                    HelpShow.ShowHelp(); // Вызов метода из HelpShow.cs
+                    break;
+                case "settings":
+                    
+                    break;
+                default:
+                    Console.WriteLine($"Неизвестный аргумент: {args[0]}");
+                    break;
+            }
+        }
+    }
+
+    static void ShowWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to Cocoa Laun! \nвведите $cocoalaun help чтобы получить справку по использованию программы");
+        Console.WriteLine("Ver:" + versi);
+        Console.WriteLine("CocoaLaun - Minecraft Server Launcher");
+    }
+}
