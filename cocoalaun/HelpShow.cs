c7769a2a5a3ec09c300bc5f5be330b99f9b6be35
Kabancik166 <kabancik166@gmail.com>
2024-09-14 12:29:42 +0300

    Add SettingsShow.cs

--- cocoalaun/HelpShow.cs
+++ cocoalaun/HelpShow.cs
@@ -10,7 +10,7 @@ public class HelpShow
         Console.WriteLine(" -$  cocoalaun help   (Help to cocoalaun) \n -$  cocoalaun -settings   (Global Settings :/) \n -$  cocoalaun -version   (View version) \n -$  cocoalaun -stopall   (Save and stop all servers)");
         Console.WriteLine("--------------------------------------------------");
         Console.WriteLine(" Server Global Commands: ");
-        Console.WriteLine(" -$  cocoalaun servers -list   (list servers) \n -$  cocoalaun servers --{id server} -open   (Open directory server) \n -$  cocoalaun servers -new   (Create new Server) \n -$  cocoalaun servers -import   (Importing Server) \n -$  cocoalaun servers --{server-id} -export   (Export {server-id}.zip to /home)  \n -$  cocoalaun servers --{id server} -delete   (Delete server) \n -$  cocoalaun servers -deleteall   (Delete all servers) \n cocoalaun servers -settings   (Global settings servers)");
+        Console.WriteLine(" -$  cocoalaun servers -list   (list servers) \n -$  cocoalaun servers --{id server} -open   (Open directory server) \n -$  cocoalaun servers -new   (Create new Server) \n -$  cocoalaun servers -import   (Importing Server) \n -$  cocoalaun servers --{server-id} -export   (Export {server-id}.zip to /home)  \n -$  cocoalaun servers --{id server} -delete   (Delete server) \n -$  cocoalaun servers -deleteall   (Delete all servers) \n -$  cocoalaun servers -settings   (Global settings servers)");
         Console.WriteLine("--------------------------------------------------");
         Console.WriteLine(" Server Editor Commands:  ");
         Console.WriteLine(" -$  cocoalaun servers editor --{id server}   (Menu Editor Server) \n -$  cocoalaun servers editor -leave   (Leave is Editor Server) ");
--- cocoalaun/SettingsShow.cs
+++ cocoalaun/SettingsShow.cs
@@ -1,57 +1,111 @@
-namespace CocoaLaun;
+using System;
+using System.IO;
 
-
-public class SettingsShow
+namespace CocoaLaun
 {
-    public static string path_to_folder_server;
-    public static void SettingsShw()
+    public class SettingsShow
     {
-        string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        public static string path_to_folder_server;
         
-        path_to_folder_server = $"{homeDirectory}/myfolder";
-
-        Console.WriteLine("Welcome to Global Settings cocoalaun! \n ");
-        Console.WriteLine("\n 1. Path to Servers cocoalaun  = " + path_to_folder_server );
-        
-        int input;
-        bool validInput = false;
-
-        while (!validInput)
+        public static void SettingsShw()
         {
-            Console.WriteLine("\nPress any number: ");
-            string inputString = Console.ReadLine();
-
-            if (string.IsNullOrEmpty(inputString))
-            {
-               
-                Console.WriteLine("Exiting...");
-                return;
-            }
+            string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             
-            if (int.TryParse(inputString, out input))
+            path_to_folder_server = $"{homeDirectory}/myfolder";
+
+            Console.WriteLine("Welcome to Global Settings cocoalaun! \n");
+            Console.WriteLine("\n 1. Path to Servers cocoalaun = " + path_to_folder_server + 
+                " (To change the path, enter only the part after /home/username. For example, for the path /home/username/Desktop, simply enter /Desktop. Other methods of specifying the path are not supported.)");
+            Console.WriteLine("15. Exit Settings    (Exit Settings:/)");
+
+            int input;
+            bool validInput = false;
+
+            while (!validInput)
             {
-                validInput = true;
-                switch (input)
+                Console.WriteLine("\nPress any number: ");
+                string inputString = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(inputString))
                 {
-                    case 1:
-                        string inputServer = Console.ReadLine();
-                        path_to_folder_server = $"{homeDirectory}{inputServer}";
-                        break;
-                    default:
-                        Console.WriteLine("\nError: Invalid input.");
-                        break;
+                    Console.WriteLine("Exiting...");
+                    return;
+                }
+                
+                if (int.TryParse(inputString, out input))
+                {
+                    switch (input)
+                    {
+                        case 1:
+                            while (true)
+                            {
+                                Console.WriteLine("\nEnter the new path (e.g., /Desktop): ");
+                                string inputServer = Console.ReadLine();
+
+                                if (string.IsNullOrEmpty(inputServer))
+                                {
+                                    Console.WriteLine("No input provided. Exiting...");
+                                    return;
+                                }
+
+                                if (IsPathValid(inputServer))
+                                {
+                                    string escapedPath = EscapeSpaces(inputServer);
+                                    path_to_folder_server = $"{homeDirectory}{escapedPath}";
+                                    Console.WriteLine("\nPath successfully updated to: " + path_to_folder_server);
+                                    
+                                    SettingsShw();
+                                    return;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nInvalid path syntax. Please try again.");
+                                }
+                            }
+                            break;
+                        case 15:
+                            validInput = true;
+                            break;
+                        default:
+                            Console.WriteLine("\nError: Invalid input.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input. Try again.");
                 }
             }
-            else
-            {
-                Console.WriteLine("\nInvalid input. Try again.");
-            }
         }
-            
-                
-        
-        
-        
-        
+
+        public static bool IsPathValid(string path)
+        {
+            char[] invalidChars = Path.GetInvalidPathChars();
+
+            foreach (char c in invalidChars)
+            {
+                if (path.Contains(c) && c != ' ')
+                {
+                    return false;
+                }
+            }
+
+            if (!path.StartsWith("/"))
+            {
+                return false;
+            }
+
+            if (path.EndsWith("\\"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string EscapeSpaces(string path)
+        {
+            return path.Replace(" ", "\\ ");
+        }
     }
 }
